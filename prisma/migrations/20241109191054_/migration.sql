cc7742c295474cf30dca8c127eb3b7835ebafd27
elder51 <elder2051@gmail.com>
2024-11-21 00:00:37 -0300

    database prisma

--- prisma/migrations/20241109191054_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `date` on the `events` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
-
-*/
--- AlterTable
-ALTER TABLE `events` MODIFY `date` DATETIME NOT NULL;
--- prisma/migrations/20241121024946_/migration.sql
+++ prisma/migrations/20241121024946_/migration.sql
@@ -32,6 +32,17 @@ CREATE TABLE `groups` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `post` (
+    `id` VARCHAR(191) NOT NULL,
+    `photo` LONGTEXT NULL,
+    `title` VARCHAR(45) NOT NULL,
+    `description` VARCHAR(1024) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `categories` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
@@ -58,11 +69,32 @@ CREATE TABLE `users_on_groups` (
     PRIMARY KEY (`user_id`, `group_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `_posts_on_events` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_posts_on_events_AB_unique`(`A`, `B`),
+    INDEX `_posts_on_events_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_posts_on_groups` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_posts_on_groups_AB_unique`(`A`, `B`),
+    INDEX `_posts_on_groups_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `events` ADD CONSTRAINT `events_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `groups` ADD CONSTRAINT `groups_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `post` ADD CONSTRAINT `post_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `users_on_events` ADD CONSTRAINT `users_on_events_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -75,3 +107,15 @@ ALTER TABLE `users_on_groups` ADD CONSTRAINT `users_on_groups_user_id_fkey` FORE
 
 -- AddForeignKey
 ALTER TABLE `users_on_groups` ADD CONSTRAINT `users_on_groups_group_id_fkey` FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_posts_on_events` ADD CONSTRAINT `_posts_on_events_A_fkey` FOREIGN KEY (`A`) REFERENCES `events`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_posts_on_events` ADD CONSTRAINT `_posts_on_events_B_fkey` FOREIGN KEY (`B`) REFERENCES `post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_posts_on_groups` ADD CONSTRAINT `_posts_on_groups_A_fkey` FOREIGN KEY (`A`) REFERENCES `groups`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_posts_on_groups` ADD CONSTRAINT `_posts_on_groups_B_fkey` FOREIGN KEY (`B`) REFERENCES `post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
